6fd72041d3ac0350a5584814eedfafa6e25e04ce
Rubén Fernández Lopesino <ruben.fernandezlopesino@ust.com>
2022-08-01 18:10:57 +0200

    router wip

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import CategoriesContainer from "./components/category-directory/categories.container";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import Home from "./routes/home/home.container";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navbar</h1>
+      </div>
+      <Outlet></Outlet>
+    </div>
+  );
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoriesContainer categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<NavBar></NavBar>}>
+        <Route index element={<Home></Home>}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
