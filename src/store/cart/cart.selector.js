7230ae0751aaaf972cf99780785a3821a9d3b6d6
Rubén Fernández Lopesino <ruben.fernandezlopesino@ust.com>
2022-11-02 20:34:29 +0100

    working without logger

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -1,6 +1,6 @@
 import { createSelector } from "reselect";
 
-const getStateCart = (state) => state.cart.cart;
+const getStateCart = (state) => state.cart;
 
 export const getIsCartOpen = createSelector(
   [getStateCart],
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -13,9 +12,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...[]));
 
 export const store = createStore(
   persistedReducer,
